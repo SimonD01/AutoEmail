19989f5f6a4292754be7d0e8727f949f8a0dee2f
Simon Denisov <simondnsv@gmail.com>
2021-08-05 11:24:12 -0700

    Send emails, added .gitignore

---
+++ config.py
@@ -0,0 +1,1 @@
+password = "lolpo2005"
--- send.py
+++ send.py
@@ -1,1 +1,17 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import config
 
+message = MIMEMultipart()
+message["from"] = "Simon Denisov"
+message["to"] = 'semsemden@gmail.com'
+message["subject"] = "Test"
+message.attach(MIMEText("Body"))
+
+with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login("simondnsv@gmail.com", config.password)
+    smtp.send_message(message)
+    print("Sent...")
